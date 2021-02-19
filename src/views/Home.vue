76cfd053c43354f308f10e42716cbc418c46cb5a
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-02-19 08:50:31 +0400

    Fix some style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,14 +8,14 @@
             v-model="body.url"
             style="width: 100%"
             :error-messages="errors.url"
-            placeholder="Insert url..."
+            placeholder="Shorten your url"
         />
         <v-btn class="mb-7 ml-3" style="width: 30%;" dark color="green" type="submit" :loading="isLoading">Shorten
         </v-btn>
       </v-form>
     </div>
-    <div class="d-flex align-center justify-center flex-column" style="height: 100%">
-      <div class="mt-16 text-center" style="width: 50%">
+    <div class="d-flex align-center justify-center flex-column" style="height: 100%;width: 100vw">
+      <div class="mt-16 text-center" style="overflow: hidden;width: 85%">
         <v-data-table
             :headers="headers"
             :items="urls"
@@ -24,29 +24,40 @@
             class="elevation-1"
             :loading="isFetchingUrls"
             no-data-text="You have no urls 🙁"
-            :footer-props="{
-      showFirstLastPage: true,
-      firstIcon: 'mdi-arrow-collapse-left',
-      lastIcon: 'mdi-arrow-collapse-right',
-      prevIcon: 'mdi-minus',
-      nextIcon: 'mdi-plus'
-    }"
         >
           <template v-slot:item.short_url="{item}">
             <v-btn :to="`/statistics/${item.short_url}`">Url</v-btn>
           </template>
 
           <template v-slot:item.action="{item}">
-            <v-btn :href="`http://localhost:8000/api/urls/${item.short_url}`" target="_blank">
-              <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
+            <div class="d-flex">
+              <v-btn class="mr-3" :href="`http://localhost:8000/${item.short_url}`"
+                     target="_blank">
+                <v-icon>mdi-open-in-new</v-icon>
+              </v-btn>
+              <button @click="copy($event,item)"
+                      style="outline: none;color:white;background-color: #4caf50;padding: 5px 10px;width:60px;border-radius: 5px;margin-right: 10px">
+                Copy
+              </button>
+              <v-btn class="delete-button" :loading="urlToBeDeleted === item.id" color="red" dark
+                     @click="deleteUrl(item)">
+                <v-icon>
+                  mdi-trash-can-outline
+                </v-icon>
+              </v-btn>
+            </div>
           </template>
 
           <template v-slot:item.total_clicks="{item}">
-            {{ item.total_clicks }}
+            {{ item.total_clicks ? item.total_clicks : 0 }}
           </template>
           <template v-slot:item.last_click="{value}">
-            <span>{{ +value ? value : '-' }}</span>
+            <div style="width: 100px">
+              <div>
+                <span v-if="value === ''">-</span>
+                <span v-else class="d-flex align-content-center justify-center"><v-icon>mdi-calendar-month</v-icon>{{ value }}</span>
+              </div>
+            </div>
           </template>
         </v-data-table>
       </div>
@@ -75,22 +86,51 @@ export default {
       hasUrls: false,
       headers: [
         {text: 'ID', value: 'id', align: 'center',},
-        {text: 'Url', value: 'url',align: 'center'},
-        {text: 'Shortened Url', value: 'short_url',align: 'center'},
-        {text: 'Action', value: 'action',align: 'center'},
-        {text: 'Total clicks', value: 'click_count',align: 'center'},
-        {text: 'Last-click', value: 'last_click',align: 'center'},
+        {text: 'Url', value: 'url', align: 'center'},
+        {text: 'Shortened Url', value: 'short_url', align: 'center'},
+        {text: 'Action', value: 'action', align: 'center'},
+        {text: 'Total clicks', value: 'click_count', align: 'center'},
+        {text: 'Last-click', value: 'last_click', align: 'center'},
       ],
       errors: [],
+      copiedUrl: '',
       urls: [],
       body: {
         url: '',
       },
       isLoading: false,
       isFetchingUrls: false,
+      urlToBeDeleted: false
     }
   },
   methods: {
+    async deleteUrl(item) {
+      try {
+        if(!confirm('Are you sure to delete?')){
+          return
+        }
+        this.urlToBeDeleted = item.id;
+        console.log(this.urlToBeDeleted);
+        await axios.delete(`api/${item.short_url}`);
+        location.reload();
+      } catch (e) {
+        //
+      } finally {
+        this.urlToBeDeleted = false;
+      }
+    },
+    copy(e, item) {
+      let input = document.createElement('input');
+      this.$el.append(input);
+      input.style.cssText = 'position:absolute;top:-999999px;left:-999999px';
+      input.value = `http://localhost:8000/${item.short_url}`;
+      console.log(e.target.value);
+      e.target.style.innerText = 'Copied!';
+      console.log(e.target.style.innerText);
+      setInterval(() => e.target.style.innerText = 'Copy', 2000);
+      input.remove();
+    }
+    ,
     async shorten() {
       try {
         this.isLoading = true;
